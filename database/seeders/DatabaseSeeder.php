8e0bdb43bb1dabfbde4f69b62b8632fdfe94d0c3
Kosrat Noori <kosratket@gmail.com>
2025-08-26 17:20:16 +0300

    01

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //  User::factory(1)->create();
+        User::factory(1)->create();
         // $this->call([
         //     EmployeeVotesTableSeeder::class,
         // ]);
